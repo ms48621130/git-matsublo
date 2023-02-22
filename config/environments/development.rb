7a1be4dd387bcbf32eaf842f1728b1da39ce3561
EC2 Default User <ec2-user@ip-172-31-35-2.ap-northeast-1.compute.internal>
2023-02-22 22:04:19 +0000

    [Add matsublo]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "5498499f4259447f9291ac4dedd24933.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
- # devise_for :admins
+  devise_for :admin,skip: [:passwords, :registrations], controllers: {
+  sessions: 'admin/sessions'
+}
   devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-  devise_for :admins,skip: [:passwords, :registrations], controllers: {
-  sessions: 'admins/sessions'
-}
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+
 end

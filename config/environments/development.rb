4423e9e6a8c6bacbb1a93e85a1355cf04c88d6ba
EC2 Default User <ec2-user@ip-172-31-3-29.ap-northeast-1.compute.internal>
2023-02-23 11:46:55 +0000

    [Add matsublo]

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,4 +74,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "5498499f4259447f9291ac4dedd24933.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.hosts << "b5b8fe6198cc4db28dd97a31b6152de8.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_21_194807) do
+ActiveRecord::Schema.define(version: 2023_02_22_205450) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2023_02_21_194807) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_users_on_email", unique: true

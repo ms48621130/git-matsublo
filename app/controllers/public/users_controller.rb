caba2c8f6f3d7ac8d73219bb8ea93ab3bb0bfbf6
EC2 Default User <ec2-user@ip-172-31-3-29.ap-northeast-1.compute.internal>
2023-02-27 20:50:05 +0000

    [Add git-matsublo]

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -4,5 +4,19 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user = current_user
+  end
+  
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to users_my_page_path
+    else
+      render :edit
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
   end
 end

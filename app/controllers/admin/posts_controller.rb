c64c6fc888a1cb54405fbb3ec8318dadfff19169
EC2 Default User <ec2-user@ip-172-31-3-29.ap-northeast-1.compute.internal>
2023-03-05 08:06:32 +0000

    [Add git-matsublo]

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -9,9 +9,10 @@ class Admin::PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
-
+    @tag_list = params[:post][:name].split(',')
     if @post.save
-     redirect_to admin_post_path(@post.id)
+      @post.save_tag(@tag_list)
+      redirect_to admin_post_path(@post.id)
     else
      render :new
     end
@@ -19,6 +20,7 @@ class Admin::PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @posts = Post.all
   end
 
   def edit
@@ -28,15 +30,17 @@ class Admin::PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
+      @tag_list = params[:post][:name].split(',')
+      @post.save_tag(@tag_list)
       redirect_to admin_post_path(@post.id)
     else
       render :edit
     end
   end
-  
+
   private
   def post_params
-    params.require(:post).permit(:image, :matsuri_name, :article, :season, :area)
+    params.require(:post).permit(:image, :matsuri_name, :article, :season, :area, :name)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   has_one_attached :image
   has_many :reviews, dependent: :destroy
   has_many :post_tags, dependent: :destroy
+  has_many :tags, through: :post_tags
   has_many :places, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
@@ -23,6 +24,26 @@ class Post < ApplicationRecord
 
   def self.area_search(area)
     where(["area like?", "#{area}"])
+  end
+
+  def save_tag(sent_tags)
+  # タグが存在していれば、タグの名前を配列として全て取得
+    current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
+    # 現在取得したタグから送られてきたタグを除いてoldtagとする
+    old_tags = current_tags - sent_tags
+    # 送信されてきたタグから現在存在するタグを除いたタグをnewとする
+    new_tags = sent_tags - current_tags
+
+    # 古いタグを消す
+    old_tags.each do |old|
+      self.tags.delete　Tag.find_by(tag_name: old)
+    end
+
+    # 新しいタグを保存
+    new_tags.each do |new|
+      new_post_tag = Tag.find_or_create_by(tag_name: new)
+      self.tags << new_post_tag
+    end
   end
 
 end

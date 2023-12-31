63a717003402d8516fc4a0326faea762aa08095d
ms48621130 <shimimasa.1130@gmail.com>
2023-12-31 13:44:57 +0000

    レビュー投稿未入力時の修正

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -19,7 +19,7 @@ class Public::ReviewsController < ApplicationController
     if @review.save
       redirect_to post_reviews_path(@post.id)
     else
-      render :show
+      render :public/posts/show
     end
   end
 

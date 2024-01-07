e9fe184c25c8650c951b340214be6736704ef5db
ms48621130 <shimimasa.1130@gmail.com>
2024-01-07 22:20:16 +0000

    エラーメッセージの日本語表示

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -69,7 +69,7 @@ class Public::RegistrationsController < Devise::RegistrationsController
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :email, :encrypted_password, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name, :email, :password, :image])
   end
 
 end

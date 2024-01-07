6930ee349b0dbefc6dce2e3f3297db58ccc05193
ms48621130 <shimimasa.1130@gmail.com>
2024-01-07 08:42:39 +0000

    エラーメッセージの日本語表示

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -61,10 +61,10 @@ class Public::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
-  def after_sign_up_path_for(resource)
-    flash[:notice] = "新規登録に成功しました"
-    user_path(@user.id)
-  end
+  #def after_sign_up_path_for(resource)
+    #flash[:notice] = "新規登録に成功しました"
+    #user_path(@user.id)
+  #end
 
   protected
 
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  
+
   # GET /resource/sign_in
   # def new
   #   super
@@ -25,14 +25,21 @@ class Public::SessionsController < Devise::SessionsController
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
 
-  def after_sign_in_path_for(resource)
-    flash[:notice] = "ログインに成功しました"
-    root_path
+  #def after_sign_in_path_for(resource)
+    #flash[:notice] = "ログインに成功しました"
+    #root_path
+  #end
+
+  #def after_sign_out_path_for(resource)
+    #flash[:notice] = "ログアウトに成功しました"
+    #root_path
+  #end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys:[:email, :encrypted_password])
   end
 
-  def after_sign_out_path_for(resource)
-    flash[:notice] = "ログアウトに成功しました"
-    root_path
-  end
 
 end

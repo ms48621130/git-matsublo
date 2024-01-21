775cede1987f38de54341c8a7807c1a7adaeddf4
ms48621130 <shimimasa.1130@gmail.com>
2024-01-21 08:53:47 +0000

    タグリストの編集

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     @posts = Post.all
-    @tags = Tag.all
+    @tags = Tag.joins(:posts).where.not(posts: { id: nil })
   end
 
   def post_search
@@ -18,12 +18,11 @@ class Public::HomesController < ApplicationController
       @posts = Post.where(matsuri_name: params[:matsuri_name])
 
     end
-    @tags = Tag.all
+    @tags = @tags = Tag.joins(:posts).where.not(posts: { id: nil })
     render "top"
   end
 
   def tag_search
-    @tags = Tag.all
     if(params[:tag_name].present?)
       @posts = Post.includes(:tags).where(tags:{tag_name: params[:tag_name]})
 #      @tags = Tag.tag_name_search(params[:tag_name])
@@ -33,6 +32,7 @@ class Public::HomesController < ApplicationController
     #@posts = Post.all
     # posttag= PostTag.where(tag_id:@tags)
     # @posts = Post.where(id:posttag)
+    @tags = Tag.joins(:posts).where.not(posts: { id: nil })
     render "top"
   end
 

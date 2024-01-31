980573ecbf69b53f65f5c37630107b7b30e06770
ms48621130 <shimimasa.1130@gmail.com>
2024-01-31 07:13:13 +0000

    タグ追加のリファクタリング

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,23 +15,13 @@ class Post < ApplicationRecord
     validates :image
   end
 
-
-  def save_tag(sent_tags)
-    # タグが存在していれば、現在のタグの名前を配列として全て取得
-    current_tags = self.tags.pluck(:tag_name) unless self.tags.nil?
-    # 送られたタグと現在のタグを合わせたものから重複を排除したものを新しいタグとする
-    new_tags = (sent_tags + current_tags).uniq
-    # 現在のタグを消す
-    current_tags.each do |tag|
-      self.tags.delete Tag.find_by(tag_name: tag)
-      sleep 1
+  def save_tag(sent_tag_names)
+    current_tag_names = self.tags.pluck(&:name)
+    new_tags = (sent_tag_names + current_tag_names).uniq.map do |tag_name|
+      tag_name = Tag.find_or_create_by(tag_name: tag_name)
     end
-    # 新しいタグを保存
-    new_tags.each do |new|
-      new_post_tag = Tag.find_or_create_by(tag_name: new)
-      sleep 2
-      self.tags << new_post_tag
-    end
+    self.tags.replace(new_tags)
   end
 
 end
+

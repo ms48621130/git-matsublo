74b8134e0cfc00434e06c9e1a748650f087b1f05
ms48621130 <shimimasa.1130@gmail.com>
2024-02-06 11:16:32 +0000

    タグ削除のリファクタリング

--- app/controllers/admin/post_tags_controller.rb
+++ app/controllers/admin/post_tags_controller.rb
@@ -9,13 +9,10 @@ class Admin::PostTagsController < ApplicationController
   end
 
   def destroy
-#    @post = Post.find(params[:post_id])
-#    @tag_list = params[:post][:name].split(',')
-#    @post.destroy(@tag_list)
-
-    post_tag = PostTag.find_by(tag_id: params[:tag_id], post_id: params[:post_id])
-    post_tag.destroy
     @post = Post.find(params[:post_id])
+    @post_tag = @post.post_tags.find(params[:id])
+    @post_tag.destroy
+    redirect_to admin_post_path(@post.id)
   end
 
 end
